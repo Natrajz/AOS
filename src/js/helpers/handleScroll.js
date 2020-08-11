1c5687afd023d79436abbd0122709638bf504a2e
Nataraj_P <natarajan.p@hotcourses.co.in>
2020-08-11 10:43:35 +0530

    code commit

---
+++ src/js/helpers/handleScroll.js
@@ -0,0 +1,59 @@
+/**
+ * Set or remove aos-animate class
+ * @param {node} el         element
+ * @param {int}  top        scrolled distance
+ * @param {void} once
+ */
+ let lastScrollTop = 0;
+const setState = function (el, top, once) {
+  const attrOnce = el.node.getAttribute('data-aos-once');
+  
+  lastScrollTop = top + 40;
+  let container = document.getElementById('transcroller-body');
+
+  if(lastScrollTop > top && (top + container.offsetHeight) > el.position)  {
+     el.node.classList.add('aos-animate');
+  }
+  else if (typeof attrOnce !== 'undefined') {
+    if (attrOnce === 'false' || (!once && attrOnce !== 'true')) {
+      el.node.classList.remove('aos-animate');
+    }
+  } 
+};
+
+
+/**
+ * Scroll logic - add or remove 'aos-animate' class on scroll
+ *
+ * @param  {array} $elements         array of elements nodes
+ * @param  {bool} once               plugin option
+ * @return {void}
+ */
+const handleScroll = function ($elements, event, once) {
+  if (event) {
+  const scrollTop = event.target.scrollTop;  
+  /**
+   * Check all registered elements positions
+   * and animate them on scroll
+   */
+  $elements.forEach((el, i) => {
+    setState(el, scrollTop, once);
+  });
+
+  } 
+  else {
+  const scrollTop = window.pageYOffset;
+  const windowHeight = window.innerHeight;
+  /**
+   * Check all registered elements positions
+   * and animate them on scroll
+   */
+  $elements.forEach((el, i) => {
+    setState(el, windowHeight + scrollTop, once);
+  });
+  }
+
+  
+};
+
+export default handleScroll;
